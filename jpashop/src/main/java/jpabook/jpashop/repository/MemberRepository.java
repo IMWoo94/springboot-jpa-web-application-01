f3fc22b9f75a690b77f170c1046ca26da8ab0359
IMWoo94 <leemeawoo94@gmail.com>
2023-10-18 18:39:28 +0900

    web part1 member domain develop and test case add 2023.10.18

--- jpashop/src/main/java/jpabook/jpashop/repository/MemberRepository.java
+++ jpashop/src/main/java/jpabook/jpashop/repository/MemberRepository.java
@@ -1,7 +1,11 @@
 package jpabook.jpashop.repository;
 
+import java.util.List;
+
 import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
 import javax.persistence.PersistenceContext;
+import javax.persistence.PersistenceUnit;
 
 import org.springframework.stereotype.Repository;
 
@@ -11,7 +15,10 @@ import jpabook.jpashop.domain.Member;
 public class MemberRepository {
 
 	@PersistenceContext
-	EntityManager em;
+	private EntityManager em;
+
+	@PersistenceUnit
+	private EntityManagerFactory emf;
 
 	public Long save(Member member) {
 		em.persist(member);
@@ -20,5 +27,16 @@ public class MemberRepository {
 
 	public Member find(Long id) {
 		return em.find(Member.class, id);
+	}
+
+	public List<Member> findAll() {
+		return em.createQuery("select m from Member m ", Member.class)
+			.getResultList();
+	}
+
+	public List<Member> findByName(String name) {
+		return em.createQuery("select m from Member m where m.username = :name ", Member.class)
+			.setParameter("name", name)
+			.getResultList();
 	}
 }
